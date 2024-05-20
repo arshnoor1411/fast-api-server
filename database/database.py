a8c47d92a1d77f4ff0a5b1d2822c3c2a5bc70da2
arshnoor1411 <arshnoor@infocusp.com>
2024-05-20 18:06:39 +0530

    fix: async session added to database

--- database/database.py
+++ database/database.py
@@ -1,15 +1,25 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import create_engine
+from contextlib import asynccontextmanager
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/fast-api-python"
+
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/fast-api"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(
+
+# Create a configured "sessionmaker" instance
+AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
 
+@asynccontextmanager
 async def get_db():
-    async with async_session() as session:
-        yield session
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
