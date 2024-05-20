4ac9a1886224ea0b97ec559bcd1eda117f3d8f8a
arshnoor1411 <arshnoor@infocusp.com>
2024-05-20 18:06:09 +0530

    fix: cors added to main file

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from database.models import Base
 
 from src.user.signup import router as user_router
+import database.models
+from database.database import engine
+import asyncio
 
 app = FastAPI()
 
 app.include_router(user_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(init_models())
+
+# database.models.Base.metadata.create_all(bind=engine)
+
 @app.get("/")
-async def read_root():
+def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
