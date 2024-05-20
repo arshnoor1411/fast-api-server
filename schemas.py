f9a319f176e347cd0ac0c6efef999e9e5f03e87b
arshnoor1411 <arshnoor@infocusp.com>
2024-05-20 18:05:55 +0530

    fix: scema fixed for required params

--- schemas.py
+++ schemas.py
@@ -3,11 +3,13 @@ from uuid import UUID
 from datetime import datetime
 
 class SignUpModel(BaseModel):
-    id: UUID
     firstname: str
     lastname: str
     email: str
     password: str
+
+class SignUp(SignUpModel):
+    id: UUID
     createdAt: datetime
     updatedAt: datetime
     deletedAt: datetime
