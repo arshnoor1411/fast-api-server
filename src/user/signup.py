c352c4f8687e9e451d6b61d82ae618317ad6025d
arshnoor1411 <arshnoor@infocusp.com>
2024-05-21 14:42:37 +0530

    fix: signup function done

--- src/user/signup.py
+++ src/user/signup.py
@@ -8,6 +8,7 @@ from database.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import func
 from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.future import select
 
 
 # def get_session():
@@ -25,21 +26,28 @@ router = APIRouter()
 
 @router.post("/signup")
 async def signup(user: schemas.SignUpModel, db: AsyncSession = Depends(get_db)):
-    print("User",user)
-    existing_user = db.query(models.User).filter_by(email = user.email).first
+    try:
+        print("User",user)
+        existing_user = await db.execute(select(models.User).filter_by(email=user.email))
+        print(existing_user)
 
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email Already Registered")
-    
-    hashPassword = bcrypt.hashpw(user.password, bcrypt.gensalt())
+        # if existing_user:
+        #   raise HTTPException(status_code=400, detail="Email already registered")
+        
+        hashPassword = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+        stringHashPassword = hashPassword.decode('utf-8')
 
-    new_user = models.User(firstname = user.firstname,lastname = user.lastname, email = user.email, password = hashPassword, createdAt = func.now(), updatedAt = func.now(), deletedAt = func.now())
+        new_user = models.User(firstname = user.firstname,lastname = user.lastname, email = user.email, password = stringHashPassword, createdAt = func.now(), updatedAt = func.now(), deletedAt = func.now())
 
-    db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user)
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
 
-    return {"message":"User Created Successfully"}
+        return {"message":"User Created Successfully"}
+    except Exception as e:
+        await db.rollback()
+        raise HTTPException(status_code=500, detail="Email already registered")
+        
 
 @router.get("/get")
 def getWorld():
