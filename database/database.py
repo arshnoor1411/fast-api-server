20f21992f0ba6cabb80fc176ccdf6517c66e4664
arshnoor1411 <arshnoor@infocusp.com>
2024-05-24 17:01:00 +0530

    fix: removed commented code

--- database/database.py
+++ database/database.py
@@ -1,7 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import create_engine
 from contextlib import asynccontextmanager
 
 
