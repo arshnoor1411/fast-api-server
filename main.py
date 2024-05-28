4e9e50abfac630f928126f20dba2c18f76bdb27d
arshnoor1411 <arshnoor@infocusp.com>
2024-05-28 17:47:29 +0530

    fix: removed unused import

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from database.models import Base
 
 from src.user.user_service import router as user_router
-import database.models
 from database.database import engine
 import asyncio
 
